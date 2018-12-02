c1a604c23a3f4e1ffac1af93ab6ee8b9b7f3c534
TianXiaoShang <1040418011@qq.com>
2018-12-03 01:45:48 +0800

    add city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,19 +1,23 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search></city-search>    
+        <city-search></city-search>   
+        <city-list></city-list>    
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header' 
 import CitySearch from './components/Search' 
+import CityList from './components/List' 
+
 
 export default {
     name:'City',
     components:{
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList
     }
 }
 </script>
