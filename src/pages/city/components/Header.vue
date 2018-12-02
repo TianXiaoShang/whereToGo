43f553e6a50759d821c94a1202e64cb811096878
TianXiaoShang <1040418011@qq.com>
2018-12-02 23:57:55 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="header">
+            <router-link to="/">
+            <div class="iconfont back-icon">&#xe624;</div>    
+            </router-link>
+                    
+            城市选择
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .header
+        position: relative
+        overflow: hidden
+        height: $headerHeight
+        line-height: $headerHeight
+        text-align: center
+        color: #fff
+        font-size: .32rem
+        background: $bgColor
+        .back-icon
+            position: absolute 
+            top: 0
+            left: 0
+            width:.64rem
+            text-align:center
+            font-size: .4rem
+            color: #fff
+
+</style>
+
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -35,6 +35,7 @@ export default {
         }
     },
     mounted(){
+        console.log('a')
         this.getHomeInfo()
     },
     methods: {
